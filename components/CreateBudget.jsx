717e4b0b7941d449f3bd1688f8c61a999c29ec46
vedXpatel <ved.gp@somaiya.edu>
2023-03-23 13:41:08 +0530

    removed material packages

--- components/CreateBudget.jsx
+++ components/CreateBudget.jsx
@@ -15,9 +15,6 @@ import {
 } from "react-native";
 import Navbar from "./NavBar";
 import DropDownPicker from "react-native-dropdown-picker";
-import Slider, { SliderThumb } from '@mui/material/Slider';
-import { styled } from '@mui/material/styles';
-import Typography from '@mui/material/Typography';
 
 const height = Dimensions.get("window").height;
 const width = Dimensions.get("window").width;
@@ -27,48 +24,6 @@ export default CreateBudget = ({ navigation, route }) => {
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
     // toggleSwitch
-
-    // slider code 
-    const PrettoSlider = styled(Slider)({
-        color: '#52af77',
-        height: 8,
-        '& .MuiSlider-track': {
-          border: 'none',
-        },
-        '& .MuiSlider-thumb': {
-          height: 24,
-          width: 24,
-          backgroundColor: '#fff',
-          border: '2px solid currentColor',
-          '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
-            boxShadow: 'inherit',
-          },
-          '&:before': {
-            display: 'none',
-          },
-        },
-        '& .MuiSlider-valueLabel': {
-          lineHeight: 1.2,
-          fontSize: 12,
-          background: 'unset',
-          padding: 0,
-          width: 32,
-          height: 32,
-          borderRadius: '50% 50% 50% 0',
-          backgroundColor: '#52af77',
-          transformOrigin: 'bottom left',
-          transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-          '&:before': { display: 'none' },
-          '&.MuiSlider-valueLabelOpen': {
-            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-          },
-          '& > *': {
-            transform: 'rotate(45deg)',
-          },
-        },
-      });
-    // slider code 
-
 
     const [category, setCategory] = useState(null);
     const [categoryOpen, setCategoryOpen] = useState(false);
@@ -211,11 +166,7 @@ export default CreateBudget = ({ navigation, route }) => {
                     {
                         isEnabled && (
                             <View >
-                                <PrettoSlider
-        valueLabelDisplay="auto"
-        aria-label="pretto slider"
-        defaultValue={20}
-      />
+                                <Text>Something</Text>
                             </View>
                         ) 
                     }
