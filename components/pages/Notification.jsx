785abb53370fa330284d5216b1816061742bfed2
vedXpatel <ved.gp@somaiya.edu>
2023-07-11 20:36:46 +0530

    :mega: : notification screen done

--- components/pages/Notification.jsx
+++ components/pages/Notification.jsx
@@ -1,29 +1,48 @@
 import React from "react";
-import { Text, View, ScrollView, SafeAreaView, Alert } from "react-native";
-import { Appbar } from "react-native-paper";
+import {Text, View, ScrollView, SafeAreaView, Alert} from "react-native";
+import {Appbar} from "react-native-paper";
+import {ListItem} from '@rneui/themed';
 
-const Notification = ({ navigation: { goBack }, route }) => {
+const Notification = ({navigation: {goBack}, route}) => {
 
     const clearAlert = () =>
         Alert.alert('Clear', 'Do you want to delete all notifications ?', [
-            {
-                text: 'Cancel',
-                onPress: () => console.log('Cancel Pressed'),
-                style: 'cancel',
-            },
-            { text: 'OK', onPress: () => console.log('OK Pressed') },
+            {text: 'Cancel', onPress: () => console.log('Cancel Pressed')},
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
         ]);
 
+    const notificationTitle = "Shopping Budget has exceeded the limit";
+    const notificationSubTitle = "Your shopping budget has exceeded the limit";
 
     return (
         <View>
-            <Appbar.Header>
-                <Appbar.BackAction onPress={() => goBack()} />
-                <Appbar.Content title="Notification" />
-                <Appbar.Action icon="delete" onPress={clearAlert} />
+            <Appbar.Header elevated mode='center-aligned'>
+                <Appbar.BackAction onPress={() => goBack()}/>
+                <Appbar.Content title="Notifications"/>
+                <Appbar.Action icon="delete" onPress={clearAlert}/>
             </Appbar.Header>
             <ScrollView>
-                <Text>Notification</Text>
+                <ListItem bottomDivider topDivider>
+                    <ListItem.Content>
+                        <ListItem.Title>{notificationTitle.length < 35 ? notificationTitle : notificationTitle.slice(0, 35) + "..."}</ListItem.Title>
+                        <ListItem.Subtitle>{notificationSubTitle.slice(0, 35) + "..."}</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <Text>19:30</Text>
+                </ListItem>
+                <ListItem bottomDivider topDivider>
+                    <ListItem.Content>
+                        <ListItem.Title>{notificationTitle.length < 35 ? notificationTitle : notificationTitle.slice(0, 35) + "..."}</ListItem.Title>
+                        <ListItem.Subtitle>{notificationSubTitle.slice(0, 35) + "..."}</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <Text>19:30</Text>
+                </ListItem>
+                <ListItem bottomDivider topDivider>
+                    <ListItem.Content>
+                        <ListItem.Title>{notificationTitle.length < 35 ? notificationTitle : notificationTitle.slice(0, 35) + "..."}</ListItem.Title>
+                        <ListItem.Subtitle>{notificationSubTitle.slice(0, 35) + "..."}</ListItem.Subtitle>
+                    </ListItem.Content>
+                    <Text>19:30</Text>
+                </ListItem>
             </ScrollView>
         </View>
     );
